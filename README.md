25474d7fe9905805b9452cb0a1019eb0a3690516
Osakana16 <pokesakana@vivaldi.net>
2024-01-08 20:08:58 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# No More Iframe Redirection
+This is an extension for chrome.
+This extension tries to stop working iframes, which show other websites.
---
+++ content.js
@@ -0,0 +1,22 @@
+﻿var i = 0;
+let checkDOMChange = function () {
+    let iframes = document.getElementsByTagName("iframe")
+    let iframe = iframes[i];
+    if (iframe !== null && iframe !== undefined) {
+        try {
+            const URL_REGEX = /(https:\/\/)(.*?)\//i;
+            let current_domain = location.href.match(URL_REGEX)[2];
+            let result = iframe.src.match(URL_REGEX)[2];
+            if (result !== "syndication.twitter.com" && result !== "www.facebook.com") {
+                if (current_domain !== result) {
+                    iframe.style.pointerEvents = "none";
+                }
+            }
+        } catch (exception) {
+            iframe.style.pointerEvents = "none";
+        }
+        i = (i + 1) % iframes.length;
+    }
+    setTimeout(checkDOMChange, 10);
+}
+checkDOMChange();
